cca335172440767e5a0ca8f03b6daeb1dd14f6a6
Gloryboyduda <duarteaagostinho@gmail.com>
2025-02-17 12:51:28 +0000

    Add acknowledgment handling for message sending in client

--- client.c
+++ client.c
@@ -6,11 +6,19 @@
 /*   By: duandrad <duandrad@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 16:57:10 by duandrad          #+#    #+#             */
-/*   Updated: 2025/02/13 23:39:09 by duandrad         ###   ########.fr       */
+/*   Updated: 2025/02/17 12:42:46 by duandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client.h"
+
+sig_atomic_t	g_ack_received = 0;
+
+void	ack_handle(int sig)
+{
+	(void)sig;
+	g_ack_received = 1;
+}
 
 void	send_char(char c, int pid)
 {
@@ -23,13 +31,18 @@ void	send_char(char c, int pid)
 			kill(pid, SIGUSR2);
 		else
 			kill(pid, SIGUSR1);
-		usleep(100);
+		usleep(1000);
+		while (!g_ack_received)
+			pause();
+		g_ack_received = 0;
 	}
 }
 
 void	send_message(char *str, int pid)
 {
-	int i = 0;
+	int	i;
+
+	i = 0;
 	while (str[i])
 	{
 		send_char(str[i], pid);
@@ -38,10 +51,10 @@ void	send_message(char *str, int pid)
 	send_char(0, pid);
 }
 
-
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	int	pid;
+
 	if (ac != 3)
 	{
 		fputstr("Invalid number of arguments\n", 1);
@@ -53,6 +66,7 @@ int main(int ac, char **av)
 		fputstr("Invalid PID\n", 1);
 		return (1);
 	}
+	signal(SIGUSR1, ack_handle);
 	send_message(av[2], pid);
 	fputstr("\n", 1);
 }
